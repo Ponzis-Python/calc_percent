9e1cef73d464f6315aa81f0ea55e17c88464ba4f
Ponzi <43649035+Ponzis-Python@users.noreply.github.com>
2019-12-05 17:10:38 -0500

    Add files via upload

---
+++ calc_percent.py
@@ -0,0 +1,8 @@
+# https://stackoverflow.com/questions/5997987/is-there-an-operator-to-calculate-percentage-in-python
+
+daPercent = float(input("Enter the percentage: "))
+daNumba = float(input("Enter the number: "))
+
+daResult = (daPercent * daNumba) / 100.0
+
+print(daResult)
